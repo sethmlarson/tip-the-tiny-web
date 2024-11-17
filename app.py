77c10b0e63f4c35382c498b049be9d07f698cb35
Seth Michael Larson <sethmichaellarson@gmail.com>
2024-11-16 19:52:39 -0600

    Add Creator and OPML import script

--- app.py
+++ app.py
@@ -1,5 +1,22 @@
-from sqlalchemy.orm import DeclarativeBase
+from typing import Optional
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session
+
+
+db_engine = create_engine("sqlite:///app.sqlite", echo=True)
+db = Session(db_engine)
 
 
 class BaseModel(DeclarativeBase):
     pass
+
+
+class Creator(BaseModel):
+    __tablename__ = "creator"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    slug: Mapped[str] = mapped_column()
+    display_name: Mapped[int] = mapped_column()
+    web_url: Mapped[str] = mapped_column()
+    feed_url: Mapped[Optional[str]] = mapped_column()
---
+++ scripts/import-from-opml.py
@@ -0,0 +1,28 @@
+"""
+Simple script which imports Creators from an OPML file.
+"""
+
+import re
+
+import app
+import opml
+
+with open("/home/sethmlarson/sethmlarson.dev/archive/feeds.opml") as f:
+    feeds_opml = opml.OpmlDocument.loads(f.read())
+
+
+def slugify(text: str):
+    return re.sub(r"[^a-z0-9]+", "-", text.lower()).strip("-")
+
+
+for outline in feeds_opml.outlines:
+    app.db.add(
+        app.Creator(
+            display_name=outline.text,
+            slug=slugify(outline.text),
+            web_url=outline.html_url,
+            feed_url=outline.xml_url or None,
+        )
+    )
+
+app.db.commit()
