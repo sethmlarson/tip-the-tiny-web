22b720af89d83c68cb9fb1b9604ca621e1b51fb4
Seth Michael Larson <sethmichaellarson@gmail.com>
2024-11-29 22:19:42 -0600

    Add budget allocation and payment states

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Tip the Tiny Web
 
 My journey into tipping the tiny web.
+
+This service is free of ads, tracking, subscriptions, fees,
+micropayments, scalping, parasitic behavior, and data walls.
+
+## How this works
+
+You set a monthly budget that you'd like to distribute to creators you enjoy.
+This budget is then distributed through a two-pool system.
+
+The first pool tracks the outstanding balance for each creator over time.
+This pool doesn't actually distribute any money, it's only there to help you
+remember how much money you'd like to distribute to each creator.
+
+The second pool is money that can be sent to creators as payments from the first pool.
+This pool has stricter requirements, such as meeting a minimum threshold for a payment platform
+or tier. This pool is cleared when you decide to pay a creator, and our service tries
+to be helpful for making these payments quick and easy.
--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-import enum
+import typing
 import urllib.parse
-from datetime import UTC, datetime
-from typing import Optional
+from datetime import UTC, datetime, timedelta
+from typing import Literal, Optional, get_args
 
-from flask import Flask, render_template
-from sqlalchemy import ForeignKey, create_engine
+from flask import Flask, make_response, render_template, request
+from sqlalchemy import ForeignKey, create_engine, func
 from sqlalchemy.ext.compiler import compiles
-from sqlalchemy.orm import DeclarativeBase, Mapped, Session, mapped_column, relationship
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Mapped,
+    Session,
+    joinedload,
+    mapped_column,
+    relationship,
+)
 from sqlalchemy.sql.functions import FunctionElement
-from sqlalchemy.types import DateTime, TypeDecorator
+from sqlalchemy.types import DateTime, Enum, TypeDecorator
 
 db_engine = create_engine("sqlite:///app.sqlite", echo=True)
 db = Session(db_engine)
@@ -81,13 +88,33 @@ class Creator(BaseModel):
     )
 
 
+class BudgetAllocation(BaseModel):
+    __tablename__ = "budget_allocations"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    supporter_id: Mapped[int] = mapped_column(
+        ForeignKey("supporters.id"), nullable=False
+    )
+    supporter: Mapped["Supporter"] = relationship(back_populates="budget_allocs")
+    allocation_amount: Mapped[int] = mapped_column(nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        type_=TzAwareDatetime, nullable=False, default=utcnow()
+    )
+
+
 class Supporter(BaseModel):
     __tablename__ = "supporters"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    supported_creators: Mapped[list["SupporterToCreator"]] = relationship()
     budget_per_month: Mapped[int] = mapped_column(nullable=False, default=0)
+    created_at: Mapped[datetime] = mapped_column(
+        type_=TzAwareDatetime, nullable=False, default=utcnow()
+    )
+    supported_creators: Mapped[list["SupporterToCreator"]] = relationship()
     payments: Mapped[list["Payment"]] = relationship(back_populates="supporter")
+    budget_allocs: Mapped[list["BudgetAllocation"]] = relationship(
+        back_populates="supporter"
+    )
 
 
 class SupporterToCreator(BaseModel):
@@ -124,10 +151,16 @@ class PaymentMethod(BaseModel):
         "polymorphic_on": "type",
     }
 
-    def refresh_from_provider(self) -> None:
-        """Refreshes the metadata from the provider,
-        such as tiers and whether one-time payments are supported.
+    def supported_payment_amounts(self) -> list[int]:
         """
+        List of possible payment amounts. An amount of '0' means
+        that one-time payments of >= self.minimum_one_time_payment_amount
+        is allowed.
+        """
+        payment_amounts = []
+        if self.supports_one_time_payments:
+            payment_amounts.append(0)
+        return payment_amounts
 
     @property
     def display_name(self) -> str:
@@ -137,6 +170,17 @@ class PaymentMethod(BaseModel):
     def html_url(self) -> str:
         """Return the web URL for the creator's payment method"""
         raise NotImplementedError()
+
+    def reify(
+        self,
+    ) -> typing.Union["GitHubSponsorsPaymentMethod", "PatreonPaymentMethod"]:
+        if self.type == "payment_methods_github_sponsors":
+            payment_cls = GitHubSponsorsPaymentMethod
+        elif self.type == "payment_methods_patreon":
+            payment_cls = PatreonPaymentMethod
+        else:
+            raise ValueError(f"Unknown PaymentMethod.type: {self.type}")
+        return db.query(payment_cls).where(payment_cls.id == self.id).first()
 
 
 class GitHubSponsorsPaymentMethod(PaymentMethod):
@@ -165,6 +209,9 @@ class PatreonPaymentMethod(PaymentMethod):
 
     __mapper_args__ = {"polymorphic_identity": "payment_methods_patreon"}
 
+    def supported_payment_amounts(self) -> list[int]:
+        return [0, 500, 1000]
+
     @property
     def display_name(self) -> str:
         return "Patreon"
@@ -174,10 +221,23 @@ class PatreonPaymentMethod(PaymentMethod):
         return f"https://patreon.com/c/{urllib.parse.quote(self.patreon_creator_slug)}"
 
 
+PaymentState = Literal["next", "unpaid", "paid"]
+
+
 class Payment(BaseModel):
     __tablename__ = "payments"
 
     id: Mapped[int] = mapped_column(primary_key=True)
+    state: Mapped[PaymentState] = mapped_column(
+        Enum(
+            *get_args(PaymentState),
+            name="payment_status",
+            create_constraint=True,
+            validate_strings=True,
+        ),
+        nullable=False,
+        default="unpaid",
+    )
     created_at: Mapped[datetime] = mapped_column(
         type_=TzAwareDatetime, nullable=False, default=utcnow()
     )
@@ -198,20 +258,279 @@ class Payment(BaseModel):
     )
 
 
+def calculate_next_budget_alloc(supporter: Supporter) -> BudgetAllocation | None:
+    recent_budget_alloc = (
+        db.query(BudgetAllocation)
+        .where(BudgetAllocation.supporter_id == supporter.id)
+        .order_by(BudgetAllocation.created_at.asc())
+        .first()
+    )
+    if recent_budget_alloc is None:
+        # This guarantees that if someone clicks the "Distribute"
+        # button on their first day, it distributes exactly their
+        # monthly budget to every creator instead of zero.
+        last_allocated_at = supporter.created_at - timedelta(days=30)
+    else:
+        last_allocated_at = recent_budget_alloc.created_at
+
+    now_in_utc = datetime.now(tz=UTC)
+    budget_per_day = int(supporter.budget_per_month * 12 // 365)
+    days_since_last_alloc = (now_in_utc - last_allocated_at).total_seconds() / (
+        24 * 60 * 60
+    )
+    alloc_amount = int(budget_per_day * days_since_last_alloc)
+    if alloc_amount < 1:
+        return None
+
+    return BudgetAllocation(
+        supporter_id=supporter.id,
+        allocation_amount=alloc_amount,
+    )
+
+
+def distribute_budget_alloc(
+    supporter: Supporter, budget_alloc: BudgetAllocation
+) -> None:
+    """Distributes an allocation of budget to creators"""
+    with db.begin(nested=True):
+        # Get all the creators that we want to pay.
+        supporter_to_creators = (
+            db.query(SupporterToCreator)
+            .where(
+                SupporterToCreator.supporter_id == supporter.id,
+                SupporterToCreator.want_to_pay,
+            )
+            # Order by creator with least outstanding to pay
+            # partial dollars to those with the least first.
+            .order_by(SupporterToCreator.payment_amount_outstanding.asc())
+            .all()
+        )
+        # Not yet paying any creators, abort!
+        if not supporter_to_creators:
+            return
+
+        # Calculate how much budget we're allocating per creator.
+        budget_remaining = budget_alloc.allocation_amount
+        budget_per_creator = int(budget_remaining // len(supporter_to_creators))
+
+        # Minimum 1 cent, we distribute to balances until the pot is gone.
+        if budget_per_creator <= 0:
+            budget_per_creator = 1
+
+        budget_allocated_to_creators = 0
+        for supporter_to_creator in supporter_to_creators:
+            # We've spent every cent, we are only gathering
+            # next outstanding at this point.
+            if budget_allocated_to_creators >= budget_remaining:
+                budget_per_creator = 0
+
+            # Calculate what a creator's next 'balance' will be
+            # given the new distribution of budget.
+            supporter_to_creator.payment_amount_outstanding += budget_per_creator
+
+        db.add(budget_alloc)
+        db.commit()
+
+
+def calculate_next_payments(supporter: Supporter) -> None:
+    with db.begin(nested=True):
+        # Delete all existing planned payments first.
+        (
+            db.query(Payment)
+            .where(Payment.supporter_id == supporter.id, Payment.state == "next")
+            .delete()
+        )
+
+        # Order Supporters by outstanding payments so those
+        # with the most balance get the first attempt to get paid.
+        creator_with_next_outstanding = sorted(
+            creator_with_next_outstanding, key=lambda x: x[1], reverse=True
+        )
+
+        # There are three payment sweeps:
+        # 1: Try to assign outstanding amounts to 'tiers'
+        # 2: TODO: Try to pay one-time-payments to fill budget
+        # 3: TODO: Pay ahead if there's remaining unpaid budget.
+
+        # Sweep 1: Assign amounts to tiers.
+        for supporter_to_creator, next_outstanding in creator_with_next_outstanding:
+            # Find all possible payment configurations.
+            # First sweep tries to assign to tiers.
+            # Second sweep tries to fill in gaps w/
+            # one-time payments.
+            creator_is_paid = False
+            for payment_method in supporter_to_creator.creator.payment_methods:
+                supported_payment_amounts = sorted(
+                    payment_method.reify().supported_payment_amounts(),
+                    reverse=True,
+                )
+                for payment_amount in supported_payment_amounts:
+                    if (
+                        payment_amount <= next_outstanding
+                        and payment_amount <= budget_remaining
+                    ):
+                        payment = Payment(
+                            supporter_id=supporter.id,
+                            payment_method_id=payment_method.id,
+                            state="next",
+                            payment_amount=payment_amount,
+                        )
+                        next_outstanding -= payment_amount
+                        budget_remaining -= payment_amount
+                        db.add(payment)
+
+                        # We've paid this creator, now we move on.
+                        creator_is_paid = True
+                        break
+
+                # for payment_method in ...
+                if creator_is_paid:
+                    break
+
+            # If there's any leftover or overpay then we save that
+            # to the creator's record for next time.
+            if next_outstanding != 0:
+                supporter_to_creator.payment_amount_outstanding = next_outstanding
+
+        db.commit()
+
+
 @web.route("/")
 def index():
-    supporter = db.query(Supporter).first()
+    supporter = db.query(Supporter).options(joinedload(Supporter.payments)).first()
     supporter_to_creators = (
         db.query(SupporterToCreator)
+        .join(Creator)
+        .options(joinedload(SupporterToCreator.creator))
         .where(SupporterToCreator.supporter_id == supporter.id)
         .all()
     )
-    creator_to_patreons = {}
-    for patreon_payment in db.query(PatreonPaymentMethod).all():
-        creator_to_patreons[patreon_payment.creator_id] = patreon_payment
-
+    supporter_to_creators = sorted(
+        supporter_to_creators,
+        key=lambda s2c: (
+            not s2c.want_to_pay,
+            s2c.creator.display_name.lower(),
+            s2c.creator.slug,
+        ),
+    )
+    next_payments = dict(
+        db.query(PaymentMethod.creator_id, func.sum(Payment.payment_amount))
+        .where(Payment.supporter_id == supporter.id, Payment.state == "next")
+        .join(PaymentMethod)
+        .group_by(PaymentMethod.creator_id)
+        .all()
+    )
+    paid_to_date = (
+        db.query(func.sum(Payment.payment_amount))
+        .where(Payment.supporter_id == supporter.id, Payment.state == "paid")
+        .scalar()
+        or 0
+    )
+    next_budget_alloc = calculate_next_budget_alloc(supporter)
+    next_budget = next_budget_alloc.allocation_amount if next_budget_alloc else 0
     return render_template(
         "index.html",
+        str=str,
+        sum=sum,
+        supporter=supporter,
         supporter_to_creators=supporter_to_creators,
-        patreons=creator_to_patreons,
+        next_budget=next_budget,
+        next_payments=next_payments,
+        paid_to_date=paid_to_date,
     )
+
+
+@web.route("/creators/<creator_slug>", methods=["GET"])
+def creator(creator_slug: str):
+    creator = (
+        db.query(Creator)
+        .options(joinedload(Creator.payment_methods))
+        .where(Creator.slug == creator_slug)
+        .first()
+    )
+    if creator is None:
+        return make_response("", 404)
+    supporter = db.query(Supporter).first()
+    supporter_to_creator = (
+        db.query(SupporterToCreator)
+        .where(
+            SupporterToCreator.creator_id == creator.id,
+            SupporterToCreator.supporter_id == supporter.id,
+        )
+        .first()
+    )
+    return render_template(
+        "creator.html", supporter_to_creator=supporter_to_creator, creator=creator
+    )
+
+
+def get_s2c_by_slug(creator_slug: str) -> SupporterToCreator | None:
+    supporter = db.query(Supporter).first()
+    supporter_to_creators = (
+        db.query(SupporterToCreator)
+        .options(joinedload(SupporterToCreator.creator))
+        .join(Creator)
+        .where(
+            SupporterToCreator.supporter_id == supporter.id,
+            Creator.slug == creator_slug,
+        )
+        .first()
+    )
+    return supporter_to_creators
+
+
+@web.route("/api/creators/<creator_slug>/want-to-pay", methods=["PUT"])
+def api_creators_want_to_pay(creator_slug: str):
+    if (supporter_to_creators := get_s2c_by_slug(creator_slug)) is None:
+        return make_response("", 404)
+    try:
+        checked = request.form["value"] == "true"
+    except KeyError:
+        checked = False
+    supporter_to_creators.want_to_pay = checked
+    db.commit()
+    return make_response("", 200)
+
+
+@web.route("/api/creators/<creator_slug>/minimum-payment-per-month", methods=["PUT"])
+def api_creators_minimum_payment_per_month(creator_slug: str):
+    if (supporter_to_creators := get_s2c_by_slug(creator_slug)) is None:
+        return make_response("", 404)
+    try:
+        # Convert to cents.
+        min_per_month = int(request.form["value"]) * 100
+        if min_per_month < 0:
+            raise ValueError("Minimum payment per month must be positive")
+    except (KeyError, ValueError):
+        return make_response("", 400)
+    supporter_to_creators.minimum_payment_per_month = min_per_month
+    db.commit()
+    return make_response("", 200)
+
+
+@web.route("/api/supporters/distribute-budget", methods=["POST"])
+def api_supporters_distribute_budget():
+    if not (supporter := db.query(Supporter).first()):
+        return make_response("", 404)
+    with db.begin(nested=True):
+        budget_alloc = calculate_next_budget_alloc(supporter)
+        if budget_alloc is None:
+            return make_response("", 200)
+        distribute_budget_alloc(supporter, budget_alloc)
+    return make_response("", 200)
+
+
+@web.route("/api/supporters/budget-per-month", methods=["PUT"])
+def api_supporters_budget_per_month():
+    if not (supporter := db.query(Supporter).first()):
+        return make_response("", 404)
+    try:
+        # Convert to cents.
+        budget_per_month = int(request.form["value"]) * 100
+        if budget_per_month < 0:
+            raise ValueError("Budget per month must be positive")
+    except (KeyError, ValueError):
+        return make_response("", 400)
+    supporter.budget_per_month = budget_per_month
+    db.commit()
+    return make_response("", 200)
